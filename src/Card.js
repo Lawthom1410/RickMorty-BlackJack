e5cb6549806cde9d85a88e258dcf620243cdc44e
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-08-18 23:30:33 +0100

    Final commit

--- src/Card.js
+++ src/Card.js
@@ -7,68 +7,98 @@ export class Card extends Component{
         this.state = {
             name: "",
             score: 0,
-            img: ""
+            img: "",
+            origin:""
         }
     }
+
+    // hideCard = () => {
+    //     this.setState({
+    //         isShown: 0
+    //     })
+    // }
 
     componentDidMount(){
         Axios.get(`https://rickandmortyapi.com/api/character/` + this.props.id).then((res) => {
             let cardData = res.data;
             let name = res.data.name;
             let score;
+            let origin;
 
             if(name === "Rick Sanchez" && cardData.origin.name === "Earth (C-137)"){
-                score = 10
+                score = 11
             } else if (name.includes("Rick")) {
-                score = 5
+                score = 8
             }
 
             else if(name === "Morty Smith" && cardData.origin.name === "Earth (C-137)"){
-                score = 8
+                score = 10
             } else if (name.includes("Morty")) {
-                score = 4
+                score = 7
             }
 
             else if((name === "Summer Smith" || name === "Beth Smith" || name === "Jerry Smith") && cardData.origin.name === "Earth (Replacement Dimension)"){
-                score = 6
+                score = 9
             } else if(name.includes("Summer") || name.includes("Beth") || name.includes("Jerry")){
-                score = 3
+                score = 6
             }
 
             else {
-                score = 1
+                score = res.data.episode.length
+            }
+
+            if(name === "Rick Sanchez" || name==="Morty Smith" || name==="Summer Smith" || name==="Beth Smith" || name==="Jerry Smith"){
+                origin = cardData.origin.name
             }
             
             this.props.updateScore(score);
 
+            
             this.setState({
-                name: cardData.name,
+                name: name,
                 score: score,
-                img: cardData.image
+                img: cardData.image,
+                origin: origin
             })
         })
+ 
     }
 
 
     render(){
         let cardStyle = {
-            width: "300px"
+            width: "200px",
+            height: "300px"
         }
+        
 
         let imgStyle = {
-            width: "250px"
+            width: "150px"
         }
-
-        return(
-            <div className="card mx-4 my-2" style={cardStyle}>
-                <div className="row card-header">
-                    <h3 className="col">{this.state.name}</h3>
-                    <h3 className="col">{this.state.score}</h3>
+        
+        if(this.props.isShown){
+            return(
+                <div className="card mx-auto my-2" style={cardStyle}>
+                    <div className="card-header">
+                        <div className="row">
+                            <p className="col">{this.state.name}</p>
+                            <h5 className="col">{this.state.score}</h5>
+                        </div>
+                    </div>
+                    <div className="card-body bg-light">
+                        <img style={imgStyle} src={this.state.img} />
+                        <small>{this.state.origin}</small>
+                    </div>
                 </div>
-                <div className="card-body">
-                    <img style={imgStyle} src={this.state.img} />
+            )
+        } else{
+            return(
+                <div className="card mx-auto my-2" style={cardStyle}>
+                    <div className="card-body bg-dark">
+                        <img style={imgStyle} src="https://i.pinimg.com/originals/0c/e9/53/0ce95363dc99e77d6210c2335b9eb23d.jpg" />
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
